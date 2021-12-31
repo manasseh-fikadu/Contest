c58e502155c0ddb5ee30eda925e5598a64fadf55
manasseh-fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2021-12-31 17:04:20 +0300

    Update 1a.py

--- 1a.py
+++ 1a.py
@@ -7,3 +7,10 @@ for i in range(1, n+1):
     for k in range(2*i -1):
         print("*", end = '')
     print("")
+   
+#Pattern 2
+n = 4
+for i in range(1, n+ 1):
+    for j in range(i):
+        print("*", end = "")
+    print("")
