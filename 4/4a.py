1faa8bfd020e0349023f1e4365b7d019044f7a67
manasseh-fikadu <80324103+manasseh-fikadu@users.noreply.github.com>
2022-01-01 09:34:44 +0300

    Create 4a.py

---
+++ 4/4a.py
@@ -0,0 +1,10 @@
+def patternGenerator():
+    rows = 3
+    for i in range(1, rows + 1):
+        print(" " * (rows - i), end=" ")
+        for j in range(1, i + 1):
+            print(j, end="")
+        for k in list(reversed(range(1, i))):
+            print(k, end="")
+        print()
+print(patternGenerator())
